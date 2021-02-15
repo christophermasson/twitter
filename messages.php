3dcfe06ce9c9c821356ba8fb34b84bd8f69d3e36
learnwithchristopherglikpo <christopherglikpoqwesi@gmail.com>
2021-02-15 11:13:58 +0100

    Modified the main.css

--- messages.php
+++ messages.php
@@ -12,6 +12,9 @@ $pageTitle="Messages / Twitter";
      <section class="mainSectionContainer">
         <div class="header-top">
            <h4>Messages</h4>
+           <a href="<?php echo url_for('messages/compose'); ?>" class="m-icon-wrapper">
+           <svg xmlns="http://www.w3.org/2000/svg" class="color-blue" viewBox="0 0 24 24"><g><path d="M23.25 3.25h-2.425V.825c0-.414-.336-.75-.75-.75s-.75.336-.75.75V3.25H16.9c-.414 0-.75.336-.75.75s.336.75.75.75h2.425v2.425c0 .414.336.75.75.75s.75-.336.75-.75V4.75h2.425c.414 0 .75-.336.75-.75s-.336-.75-.75-.75zm-3.175 6.876c-.414 0-.75.336-.75.75v8.078c0 .414-.337.75-.75.75H4.095c-.412 0-.75-.336-.75-.75V8.298l6.778 4.518c.368.246.79.37 1.213.37.422 0 .844-.124 1.212-.37l4.53-3.013c.336-.223.428-.676.204-1.012-.223-.332-.675-.425-1.012-.2l-4.53 3.014c-.246.162-.563.163-.808 0l-7.586-5.06V5.5c0-.414.337-.75.75-.75h9.094c.414 0 .75-.336.75-.75s-.336-.75-.75-.75H4.096c-1.24 0-2.25 1.01-2.25 2.25v13.455c0 1.24 1.01 2.25 2.25 2.25h14.48c1.24 0 2.25-1.01 2.25-2.25v-8.078c0-.415-.337-.75-.75-.75z"/></g></svg>
+          </a>
            
         </div>
         
@@ -19,8 +22,14 @@ $pageTitle="Messages / Twitter";
      
        
      </section>
-     <aside role="complementary">
-        Aside
+     <aside role="complementary" class="right-msg">
+        <div class="no-msg-container">
+            <div class="n-msg-wrapper">
+                <h2>You don't have a message selected</h2>
+                <p>Choose one from your existing messages or start a new one.</p>
+                <a href="<?php echo url_for("messages/compose") ?>" class="n-msg" role="button" data-focusable="true">New message</a>
+            </div>
+        </div>
      </aside>
    </main>
 </section>
